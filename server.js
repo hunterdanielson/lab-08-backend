39bb0cd7c43064491eb74a99c43d2a6f9c4dcb29
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-08 17:34:15 -0700

    posting to backend

--- server.js
+++ server.js
@@ -21,9 +21,25 @@ app.get('/weapons/:id', async(req, res) => {
   WHERE id=${req.params.id};
   `);
 
-
-
   res.json(data.rows);
+});
+
+app.post('/weapons/', async(req, res) => {
+  console.log(req.body, res.body);
+  try {
+    const data = await client.query(
+      `insert into weapons (name, attack, affinity, element, is_longsword, user_id)
+      values ($1, $2, $3, $4, $5, $6)
+      returning *;`,
+      [req.body.name, req.body.attack, req.body.affinity, req.body.element, req.body.is_longsword, 1]
+    );
+
+    res.json(data.rows[0]);
+
+  } catch(e) {
+    console.error(e);
+    res.json(e);
+  }
 });
 
 app.listen(PORT, () => {
