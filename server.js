4892b8f11513bb34a65b757901017cb4f0f7d31c
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-11 22:40:33 -0700

    deletion working

--- server.js
+++ server.js
@@ -10,35 +10,66 @@ const app = require('./lib/app');
 const PORT = process.env.PORT || 7890;
 
 app.get('/weapons', async(req, res) => {
-  // const data = await client.query('SELECT * from weapons');
-  const data = await client.query(`
-  SELECT weapons.id, weapons.name, weapons.attack, weapons.affinity, elements.element, weapons.is_longsword
-  from weapons
-  join elements
-  on weapons.element_id = elements.id
-  `);
+  try {
 
+    const data = await client.query(`
+    SELECT weapons.id, weapons.name, weapons.attack, weapons.affinity, elements.element, weapons.is_longsword
+    from weapons
+    join elements
+    on weapons.element_id = elements.id
+    `);
 
-  res.json(data.rows);
+    res.json(data.rows);
+
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({
+      error: e.message || e
+    });
+  }
 });
 
 app.get('/weapons/:id', async(req, res) => {
   const id = req.params.id;
-  const data = await client.query(`
-  SELECT weapons.id, weapons.name, weapons.attack, weapons.affinity, elements.element, weapons.is_longsword
-  from weapons
-  join elements
-  on weapons.element_id = elements.id
-  where weapons.id = $1
-  `, [id]);
+  try {
 
-  res.json(data.rows);
+    const data = await client.query(`
+    SELECT weapons.id, weapons.name, weapons.attack, weapons.affinity, elements.element, weapons.is_longsword
+    from weapons
+    join elements
+    on weapons.element_id = elements.id
+    where weapons.id = $1
+    `, [id]);
+  
+    const weapon = data.rows;
+    if(!weapon) {
+      res.status(404).json({
+        error: `Weapon id ${id} does not exist`
+      });
+    } else {
+      res.json(data.rows[0]);
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({
+      error: e.message || e
+    });
+  }
 });
 
 app.get('/elements', async(req, res) => {
-  const data = await client.query('SELECT * from elements');
+  try {
 
-  res.json(data.rows);
+    const data = await client.query('SELECT * from elements');
+  
+    res.json(data.rows);
+
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({
+      error: e.message || e
+    });
+  }
 });
 
 app.post('/weapons/', async(req, res) => {
@@ -62,6 +93,31 @@ app.post('/weapons/', async(req, res) => {
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+});
+
+app.delete('/weapons/:id', async(req, res) => {
+  const id = req.params.id;
+  try {
+
+    const data = await client.query(`
+    DELETE FROM weapons *
+    WHERE weapons.id = $1
+    `, [id]);
+  
+    const weapon = data.rows;
+    if(!weapon) {
+      res.status(404).json({
+        error: `Weapon id ${id} does not exist`
+      });
+    } else {
+      res.json(data.rows[0]);
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({
+      error: e.message || e
+    });
+  }
 });
 
 module.exports = app;
